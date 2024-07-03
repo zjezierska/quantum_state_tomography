9b83e32f9ec8fca0d2d9c13a3541d73a45077ba2
Zuzanna Jezierska <z.jezierska2@student.uw.edu.pl>
2024-07-03 15:20:25 +0200

    added checking the matrix physical properties

--- tests/test_mle.py
+++ tests/test_mle.py
@@ -15,8 +15,20 @@ class TestMLEReconstructor(unittest.TestCase):
         self.assertIsNotNone(self.mle_reconstructor.trajectories)
         
     def test_mle_algorithm(self):
-        self.mle_reconstructor.mle_algorithm()
-        self.assertIsNotNone(self.mle_reconstructor.estimated_states)
+        theta_data = np.linspace(0, 2 * np.pi, self.mle_reconstructor.trajectories.shape[1])
+        sample = self.mle_reconstructor.trajectories[0]
+        final_rho, likelihood_trend = self.mle_reconstructor.mle_algorithm(sample, theta_data, dims=2, num_iters=10)
+
+        # Check if trace of rho equals 1
+        self.assertTrue(np.isclose(np.trace(final_rho), 1), "The trace of the final density matrix is not equal to 1.")
+        
+        # Check if rho is Hermitian
+        self.assertTrue(np.allclose(final_rho, final_rho.conj().T), "The final density matrix is not Hermitian.")
+        
+        # Check if rho is positive semi-definite
+        eigenvalues = np.linalg.eigvals(final_rho)
+        self.assertTrue(np.all(eigenvalues >= 0), f"The final density matrix is not positive semi-definite. Eigenvalues: {eigenvalues}")
+
         
     def test_calculate_likelihood(self):
         likelihood = self.mle_reconstructor.calculate_likelihood(np.random.rand(4, 4), np.random.rand(10, 50, 100))
